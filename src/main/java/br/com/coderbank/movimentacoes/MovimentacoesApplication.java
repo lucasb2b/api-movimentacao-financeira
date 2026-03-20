c41c41e59f9fb8703f0b7a6651a9438f0007b02a
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2026-03-20 20:51:46 -0300

    first commit

---
+++ src/main/java/br/com/coderbank/movimentacoes/MovimentacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.movimentacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimentacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimentacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coderbank/movimentacoes/MovimentacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.movimentacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovimentacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
